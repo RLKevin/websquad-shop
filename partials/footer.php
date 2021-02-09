8dfea2014cb29881def62151bb841f4f5d4150e6
Kevin van Nieukerke <kevinvannieukerke@Kevins-MacBook-Pro.local>
2021-02-09 12:34:12 +0100

    changed footer to default websquad footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,105 +1,221 @@
-
-<?php
+<?php 
 
 // vars
-$map = get_field('options_footer_map', 'option');
-$logo = get_field('options_logo_alt', 'option');
-$icon = get_field('options_icon', 'option');
-$background = get_field('options_footer_background', 'option');
-$icon = get_field('options_icon', 'options');
 
-$location = get_field('options_map', 'option');
-$street = get_field('options_street', 'options');
-$postal_code = get_field('options_postal_code', 'options');
-$city = get_field('options_city', 'options');
-
-$phone = get_field('options_phone_number', 'option');
-$phoneStripped = str_replace([' ', '-'], '', $phone);
-$email = get_field('options_email_address', 'option');
-
-$facebook = get_field('options_social_facebook', 'options');
-$twitter = get_field('options_social_twitter', 'options');
-$instagram = get_field('options_social_instagram', 'options');
-$linkedin = get_field('options_social_linkedin', 'options');
-$youtube = get_field('options_social_youtube', 'options');
-
+$image = get_field('options_logo_alt', 'option');
+$facebook = get_field('options_facebook_link', 'option');
+$twitter = get_field('options_twitter_link', 'option');
+$linkedin = get_field('options_linkedin_link', 'option');
+$instagram = get_field('options_instagram_link', 'option');
+$youtube = get_field('options_youtube_link', 'option');
+$privacy = get_field('options_privacy', 'option');
+$intranet = get_field('options_login', 'option');
+$street = get_field('options_street', 'option');
+$postal_code = get_field('options_postal_code', 'option');
+$city = get_field('options_city', 'option');
+	
 ?>
 
-<footer id="footer">
+<footer id="footer" class="footer">
 
-	<div class="wrapper">
+	<div class="footer__topmenu">
 
-		<div class="column logo">
-	
-			<a href="<?= site_url(); ?>" class="logo">
-				<img src="<?= $logo['url']; ?>" alt="logo">
-			</a>
+		<div class="wrapper">
 
-		</div>
-		<div class="column contact">
+			<div class="footer__logo">
 
-			<img src="<?= $icon['url']; ?>" alt="logo">
+				<a href="<?php echo home_url(); ?>">
 
-			<div class="row">
-				<div class="address">
-					<p>
-						<?= $street; ?>
-					</p>
-					<p>
-						<?= $postal_code; ?> <?= $city; ?>
-					</p>
-				</div>
+					<?php if ($image): ?>
 
-				<div class="contact">
-					<a href="tel:<?= $phoneStripped; ?>" class="phone"><?= $phone; ?></a>
-					<a href="mailto:<?= $email; ?>" class="phone"><?= $email; ?></a>
-				</div>
+						<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['title']; ?>">
+
+					<?php endif; ?>
+
+				</a>
+				
+			</div>
+
+			<div class="footer__social">
+
+				<ul>
+					
+					<?php if ($facebook): ?>
+
+						<li>
+
+							<a class="facebook" href="<?php echo $facebook; ?>" target="_blank"></a>
+
+						</li>
+
+					<?php endif ?>
+
+					<?php if ($twitter): ?>
+
+						<li>
+
+							<a class="twitter" href="<?php echo $twitter; ?>" target="_blank"></a>
+
+						</li>
+
+					<?php endif ?>
+
+					<?php if ($linkedin): ?>
+
+						<li>
+
+							<a class="linkedin" href="<?php echo $linkedin; ?>" target="_blank"></a>
+
+						</li>
+
+					<?php endif ?>
+
+					<?php if ($instagram): ?>
+
+						<li>
+
+							<a class="instagram" href="<?php echo $instagram; ?>" target="_blank"></a>
+
+						</li>
+
+					<?php endif ?>
+
+					<?php if ($youtube): ?>
+
+						<li>
+
+							<a class="youtube" href="<?php echo $youtube; ?>" target="_blank"></a>
+
+						</li>
+
+					<?php endif ?>
+
+				</ul>
+
 			</div>
 
 		</div>
 
-		
+	</div>
 
-		<div class="column social">
-			<p><?= get_field('options_social', 'options'); ?></p>
-			<?php if ($facebook) { ?>
-				<a href="<?= $facebook; ?>" class="facebook" target="_blank"></a>
-			<?php } ?>
-			<?php if ($twitter) { ?>
-				<a href="<?= $twitter; ?>" class="twitter" target="_blank"></a>
-			<?php } ?>
-			<?php if ($instagram) { ?>
-				<a href="<?= $instagram; ?>" class="instagram" target="_blank"></a>
-			<?php } ?>
-			<?php if ($linkedin) { ?>
-				<a href="<?= $linkedin; ?>" class="linkedin" target="_blank"></a>
-			<?php } ?>
-			<?php if ($youtube) { ?>
-				<a href="<?= $youtube; ?>" class="youtube" target="_blank"></a>
-			<?php } ?>
+	<div class="footer__mainmenu">
+
+		<div class="wrapper">
+
+			<div class="footer__social">
+
+				<ul>
+					
+					<?php if ($facebook): ?>
+
+						<li>
+
+							<a class="facebook" href="<?php echo $facebook; ?>" target="_blank"></a>
+
+						</li>
+
+					<?php endif ?>
+
+					<?php if ($twitter): ?>
+
+						<li>
+
+							<a class="twitter" href="<?php echo $twitter; ?>" target="_blank"></a>
+
+						</li>
+
+					<?php endif ?>
+
+					<?php if ($linkedin): ?>
+
+						<li>
+
+							<a class="linkedin" href="<?php echo $linkedin; ?>" target="_blank"></a>
+
+						</li>
+
+					<?php endif ?>
+
+					<?php if ($instagram): ?>
+
+						<li>
+
+							<a class="instagram" href="<?php echo $instagram; ?>" target="_blank"></a>
+
+						</li>
+
+					<?php endif ?>
+
+					<?php if ($youtube): ?>
+
+						<li>
+
+							<a class="youtube" href="<?php echo $youtube; ?>" target="_blank"></a>
+
+						</li>
+
+					<?php endif ?>
+
+				</ul>
+
+			</div>
+
+			<div class="footer__disclaimer">
+
+				<ul>
+
+					<?php if ($privacy): ?>
+
+						<li>
+
+							<a href="<?php echo $privacy['url']; ?>" target="<?php echo $privacy['target']; ?>"><?php echo $privacy['title']; ?></a>
+						
+						</li>
+
+					<?php endif; ?>
+
+					<?php if ($intranet): ?>
+
+						<li>
+
+							<a href="<?php echo $intranet['url']; ?>" target="<?php echo $intranet['target']; ?>"><?php echo $intranet['title']; ?></a>
+						
+						</li>
+
+					<?php endif; ?>
+
+				</ul>
+
+				<ul class="address">
+
+					<?php if ($street && $postal_code && $city): ?>
+						
+						<li><?php echo $street; ?></li>
+						<li><?php echo $postal_code; ?></li>
+						<li><?php echo $city; ?></li>
+
+					<?php endif; ?>
+				
+				</ul>
+
+				<ul>
+
+					<li>
+
+						<a href="https://websquad.nl/" target="_blank">
+
+							Websquad © <?php echo date("Y"); ?>
+
+						</a>
+
+					</li>
+
+				</ul>
+
+			</div>
 
 		</div>
 
-</div>
+	</div>
 
 </footer>
-
-<section class="privacy">
-	<div class="wrapper">
-		<div class="switch">
-			Webdesign & development <a href="#">Switch Reclamebureau</a>
-		</div>
-		<div class="privacy">
-			<?php
-				$terms = get_field('options_terms', 'options');
-				$privacy = get_field('options_privacy', 'options');
-			?>
-			<?php if ($terms) { ?>
-				<a href="<?= $terms['url']; ?>"><?= $terms['title']; ?></a>
-			<?php } ?>
-			<?php if ($privacy) { ?>
-				<a href="<?= $privacy['url']; ?>"><?= $privacy['title']; ?></a>
-			<?php } ?>
-		</div>
-	</div>
-</section>
